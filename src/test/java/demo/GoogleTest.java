b4119552acab4ac2dbb7100c3f52802ada42a0ef
Eternal-66 <ayushchalekar@acpce.ac.in>
2025-09-08 13:28:00 +0530

    Update GoogleTest.java

--- src/test/java/demo/GoogleTest.java
+++ src/test/java/demo/GoogleTest.java
@@ -5,14 +5,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class GoogleTest {
+
     @Test
     public void openGoogle() {
-        // Set ChromeDriver path if not in system PATH
-        // System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        // Set ChromeDriver path if needed
+        // System.setProperty("webdriver.chrome.driver", "C:\\path\\to\\chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
-        System.out.println("Page title is: " + driver.getTitle());
+        System.out.println("Page title: " + driver.getTitle());
         driver.quit();
     }
 }
